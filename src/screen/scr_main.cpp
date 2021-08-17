9fe7061eebe527b9d2afe00b011c2a74c4b62717
Ilan Malkin <malkin.ilan@gmail.com>
2021-08-17 22:01:17 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 int main()
 {
+	/*
+	// Generates 6x6 screen with * in (2,1) and (2,2); - from (3,3) to (3,5). Background of #s
 	auto myScreen = Screen{6,6};
-	myScreen.forward();
+	myScreen.forward(); 
 	myScreen.set('*');
 	myScreen.down();
 	myScreen.set('*');
@@ -17,16 +19,41 @@ int main()
 	myScreen.display();
 	cout << endl;
 
+	// resiszes to 16x16
 	myScreen.reSize(16,16);
 	myScreen.display();
+	// clears screen and changes background to blank squares
 	myScreen.clear(' ');
 
+	// BIG on (7,7) to (7,9). SREEEN on (8,5) to (8,10)
 	myScreen.move(7,7);
 	myScreen.set("BIG");
 	myScreen.move(8,5);
 	myScreen.set("SCREEN");
 	myScreen.display();
+	*/
 
+	// display letter 'I' on 6x6 screen
+	auto myScreen = Screen{ 6,6, ' '};
+	string rowOfStars = "";
+	for (int i = 1; i <= myScreen.width(); i++)
+		rowOfStars += "*";
+	
+	// put stars in top and bottom rows
+	myScreen.home();
+	myScreen.set(rowOfStars);
+	myScreen.move(myScreen.height(), 1);
+	myScreen.set(rowOfStars);
+	
+	// put stars in middle column (or middle 2 columns if screen width is even)
+	int cursorRow = 2;
+	string colStars = (myScreen.width() % 2 == 1 ? "*" : "**");
+	while (cursorRow < myScreen.height()) {
+		myScreen.move(cursorRow, (myScreen.width() + 1) / 2);
+		myScreen.set(colStars);
+		cursorRow++;
+	}
+	myScreen.display();
 	return 0;
 }
 
